5b1d6a1c5b6d7654ac7ea4c866459dd8cc70accf
Ashish Kumar Singh <code.aks.010@gmail.com>
2020-04-04 11:55:22 +0530

    "Merge commits"

--- server/Users.js
+++ server/Users.js
@@ -1,168 +1,145 @@
-const express = require('express')
-const users = express.Router()
-const cors = require('cors')
-const axios = require('axios')
+const express = require("express");
+const users = express.Router();
+const cors = require("cors");
+const axios = require("axios");
 // const jwt = require('jsonwebtoken')
 // const bcrypt = require('bcrypt')
-const request = require('request')
+const request = require("request");
 
 // const User = require('../models/User')
-users.use(cors())
+users.use(cors());
 
-process.env.SECRET_KEY = 'secret'
+process.env.SECRET_KEY = "secret";
+const baseURL =
+  process.env.MODE == "SHARED_SERVER" ? "10.42.0.1" : "http://localhost";
 
-users.post('/register', (req, res) => {  
-  const today = new Date()
+users.post("/register", (req, res) => {
+  const today = new Date();
   const userData = {
-    firstname: req.body.first_name || 'AShish',
-    lastname: req.body.last_name ||'kumar',
+    firstname: req.body.first_name || "AShish",
+    lastname: req.body.last_name || "kumar",
     // gender: req.body.gender,
-    email: req.body.email || 'sds@af.af',
-    password: req.body.password || '123456789',
+    email: req.body.email || "sds@af.af",
+    password: req.body.password || "123456789",
     joining: req.body.joining,
-    address: req.body.address || 'asdgfhgjk',
-    status: req.body.status || 'inactive',
-    mobileno: req.body.mobile || '1234567789',
-    role: req.body.role || 'admin'
-  }
+    address: req.body.address || "asdgfhgjk",
+    status: req.body.status || "inactive",
+    mobileno: req.body.mobile || "1234567789",
+    role: req.body.role || "admin"
+  };
 
   axios
-  .post('http://localhost:8000/team/add-member',userData)
-  .then(response => {
-    console.log(response.status)
-      res.status(response.status).send(response.data)
-  })  
-  .catch(e=> {
-    console.log("ERROR")
-      console.log(e)
-    res.status(500).send(e)
-  }
-    )
-  console.log("Register request received in node")
-  console.log("Name: "+userData.first_name)
-}
-)
+    .post(`${baseURL}:8000/team/add-member`, userData)
+    .then(response => {
+      console.log(response.status);
+      res.status(response.status).send(response.data);
+    })
+    .catch(e => {
+      console.log("ERROR");
+      console.log(e);
+      res.status(500).send(e);
+    });
+  console.log("Register request received in node");
+  console.log("Name: " + userData.first_name);
+});
 
-
-users.post('/update-member', (req, res) => {  
-  const today = new Date()
+users.post("/update-member", (req, res) => {
+  const today = new Date();
   const userData = {
-    firstname: req.body.first_name || 'Ashish',
-    lastname: req.body.last_name ||'kumar',
+    firstname: req.body.first_name || "Ashish",
+    lastname: req.body.last_name || "kumar",
     // gender: req.body.gender,
-    email: req.body.email || 'sds@af.af',
-    password: req.body.password || '123456789',
+    email: req.body.email || "sds@af.af",
+    password: req.body.password || "123456789",
     joining: req.body.joining,
-    address: req.body.address || 'asdgfhgjk',
-    status: req.body.status || 'inactive',
-    mobileno: req.body.mobile || '1234567789',
-    role: req.body.role || 'admin'
-  }
+    address: req.body.address || "asdgfhgjk",
+    status: req.body.status || "inactive",
+    mobileno: req.body.mobile || "1234567789",
+    role: req.body.role || "admin"
+  };
 
   axios
-  .post('http://localhost:8000/team/update-team-member',userData, {
-    'Authorization': `Bearer ${req.headers.token}`
-  })
-  .then(response => {
-    console.log(response.status)
-      res.status(response.status).send(response.data)
-  })  
-  .catch(e=> {
-    console.log("ERROR")
-      console.log(e)
-    res.status(500).send(e)
-  }
-    )
-  console.log("Register request received in node")
-  console.log("Name: "+userData.first_name)
-}
-)
+    .post(`${baseURL}:8000/team/update-team-member`, userData, {
+      Authorization: `Bearer ${req.headers.token}`
+    })
+    .then(response => {
+      console.log(response.status);
+      res.status(response.status).send(response.data);
+    })
+    .catch(e => {
+      console.log("ERROR");
+      console.log(e);
+      res.status(500).send(e);
+    });
+  console.log("Register request received in node");
+  console.log("Name: " + userData.first_name);
+});
 
-
-
-
-
-
-users.post('/login', (req, res) => {
+users.post("/login", (req, res) => {
   const userData = {
     email: req.body.email,
     password: req.body.password
-  }
+  };
 
   axios
-  .post('http://localhost:8000/team/login',userData)
-  .then(response => {
-      res.status(response.status).send(response.data)
-  })
-  .catch(e=> {
-    console.log(e) 
-    res.status(500).send("Error: "+e)})
-})
+    .post(`${baseURL}:8000/team/login`, userData)
+    .then(response => {
+      res.status(response.status).send(response.data);
+    })
+    .catch(e => res.status(500).send("Error: " + e));
+});
 
-users.post('/password', (req, res) => {
+users.post("/password", (req, res) => {
   const userData = {
     email: req.body.email,
     password: req.body.password
-  }
-  console.log('Password change request Received!')
+  };
+  console.log("Password change request Received!");
 
   axios
-  .post('http://localhost:8000/team/password',userData)
-  .then(response => {
-      res.status(response.status).send(response.data)
-  })
-  .catch(e=> res.status(500).send("Error: "+e))
-})
+    .post(`${baseURL}:8000/team/password`, userData)
+    .then(response => {
+      res.status(response.status).send(response.data);
+    })
+    .catch(e => res.status(500).send("Error: " + e));
+});
 
-
-
-
-users.get('/view-member', (req, res) => {
+users.get("/view-member", (req, res) => {
   const userData = {
     email: req.body.email,
     password: req.body.password
-  }
+  };
   // console.log(req.headers.token)
 
   axios
-  .get('http://localhost:8000/team/view-member', {
-    headers: {
-      'Authorization': `Bearer ${req.headers.token}`
-    }
-  })
-  .then(response => {
-    console.log(response.status)
-      res.status(response.status).send(response.data)
-  })
-  .catch(e=> res.status(500).send("Error: "+e))
+    .get(`${baseURL}:8000/team/view-member`, {
+      headers: {
+        Authorization: `Bearer ${req.headers.token}`
+      }
+    })
+    .then(response => {
+      console.log(response.status);
+      res.status(response.status).send(response.data);
+    })
+    .catch(e => res.status(500).send("Error: " + e));
+});
 
-})
-
-
-
-users.get(`/team-list`, (req, res) => {
-  console.log("pagination request received in node " + req.query.page + " and countsPerPage is "+req.query.limit);
+users.get("/team-list", (req, res) => {
   axios
-  .get(`http://localhost:8000/team/list?page=${req.query.page}&limit=${req.query.limit}`, {
-    headers: {
-      'Authorization': `Bearer ${req.headers.token}`
-    }
-  })
-  .then(response => {
-      res.status(response.status).send(response.data)
-  })
-  .catch(e=> res.status(500).send("Error: "+e))
-})
+    .get(`${baseURL}:8000/team/list`, {
+      headers: {
+        Authorization: `Bearer ${req.headers.token}`
+      }
+    })
+    .then(response => {
+      res.status(response.status).send(response.data);
+    })
+    .catch(e => res.status(500).send("Error: " + e));
+});
 
+users.get("/profile", (req, res) => {});
 
-
-
-
-users.get('/profile', (req, res) => {
-
-})
-
-users.post('/update', (req, res) => {  
+users.post("/update", (req, res) => {
   // const today = new Date()
   const userData = {
     first_name: req.body.first_name,
@@ -174,18 +151,20 @@ users.post('/update', (req, res) => {
     address: req.body.address,
     status: req.body.status,
     mobile: req.body.mobile
-  }
-  
-  request.post('http://localhost:8000/api/users', userData, function (error, response, body) {
+  };
+
+  request.post(`${baseURL}:8000/api/users`, userData, function(
+    error,
+    response,
+    body
+  ) {
     if (!error && response.statusCode == 200) {
-      console.log(body)
+      console.log(body);
     }
-  })
+  });
 
-  console.log("Update request received in node")
-  console.log("Name: "+userData.first_name)
+  console.log("Update request received in node");
+  console.log("Name: " + userData.first_name);
+});
 
-})
-
-
-module.exports = users
+module.exports = users;
--- src/views/Team/UserFunctions.js
+++ src/views/Team/UserFunctions.js
@@ -1,166 +1,177 @@
-import axios from 'axios'
+import axios from "axios";
 
-export const register = newUser=>{
-  console.log("axios worked")
-  // console.log(url)
-  return axios.post('http://localhost:5000/users/register',{
-        first_name: newUser.first_name,
-        last_name: newUser.last_name,
-        gender: newUser.gender,
-        email: newUser.email,
-        password: newUser.password,
-        joining: newUser.joining,
-        address: newUser.address,
-        status: newUser.status,
-        role: newUser.role,
-        mobile: newUser.mobile  
-      }).then(response=>{
-          console.log('Registerd')
-          // console.log(response)
-      }).catch(e=>console.log(e))
-}
+const baseURL =
+  process.env.MODE == "SHARED_SERVER" ? "10.42.0.1" : "http://localhost";
 
+export const register = newUser => {
+  console.log("axios worked");
+  return axios
+    .post(`${baseURL}:5000/users/register`, {
+      first_name: newUser.first_name,
+      last_name: newUser.last_name,
+      gender: newUser.gender,
+      email: newUser.email,
+      password: newUser.password,
+      joining: newUser.joining,
+      address: newUser.address,
+      status: newUser.status,
+      role: newUser.role,
+      mobile: newUser.mobile
+    })
+    .then(response => {
+      console.log("Registerd");
+      // console.log(response)
+    })
+    .catch(e => console.log(e));
+};
 
-export const search = searchUser=>{
+export const search = searchUser => {
   // console.log("axios worked")
-  console.log("search reuested sending to node")
-  return axios.post('http://localhost:5000/users/search',{
-        id:searchUser.id,
-        first_name: searchUser.first_name,
-        last_name: searchUser.last_name,
-        // gender: searchUser.gender,
-        email: searchUser.email,
-        // password: searchUser.password,
-        joining: searchUser.joining,
-        address: searchUser.address,
-        status: searchUser.status,
-        role: searchUser.role,
-        mobile: searchUser.mobile  
-      }).then(response=>{
-          console.log('search')
-          // console.log(response)
-      }).catch(e=>console.log(e))
-}
-export const updateMember = user=>{
-  console.log("axios worked")
+  console.log("search reuested sending to node");
+  return axios
+    .post("http://localhost:5000/users/search", {
+      id: searchUser.id,
+      first_name: searchUser.first_name,
+      last_name: searchUser.last_name,
+      // gender: searchUser.gender,
+      email: searchUser.email,
+      // password: searchUser.password,
+      joining: searchUser.joining,
+      address: searchUser.address,
+      status: searchUser.status,
+      role: searchUser.role,
+      mobile: searchUser.mobile
+    })
+    .then(response => {
+      console.log("search");
+      // console.log(response)
+    })
+    .catch(e => console.log(e));
+};
+export const updateMember = user => {
+  console.log("axios worked");
   // console.log(url)
-  return axios.post('http://localhost:5000/users/update-member',{
+  return axios
+    .post(
+      `${baseURL}:5000/users/update-member`,
+      {
         first_name: user.firstname,
         last_name: user.lastname,
-        gender: user.gender || 'M',
+        gender: user.gender || "M",
         email: user.email,
         joining: user.Joining_Date,
         address: user.address,
         status: user.status,
         role: user.role,
-        mobile: user.mobile  
-      },{
+        mobile: user.mobile
+      },
+      {
         headers: {
-          token: localStorage.getItem('token')
+          token: localStorage.getItem("token")
         }
-      } ).then(response=>{
-          if(response.status===200)
-            console.log('Updated')
-          // console.log(response)
-      }).catch(e=>{
-        window.alert('Error: '+ e)
-      })
-}
-
-
-export const fetchUserDetails = (token)=>{
-  return axios.get('http://localhost:5000/users/profile',{
-        headers: {
-          token: token
-        }        
-  }).then(response=>{
-          console.log('Fetching data')
-      })
-}
-
-
-
-export const fetchTeamDetails = (token)=>{
-  return axios.get('http://localhost:5000/users/view-member',{
-        headers: {
-          token: token
-        }        
-  }).then(response=>{
-      console.log(response.status)
-      if(response.status===200){ 
-          console.log("Entered")
-           }
-      else {
-          localStorage.removeItem('token')
-          window.location.href='/login'
       }
-      })
-      .catch(e=>{
-        console.log("Error" + e)
-        window.location.href='/logout'
-        // res.status(500).send(e)
-        // console.log("error")
-      })
-}
+    )
+    .then(response => {
+      if (response.status === 200) console.log("Updated");
+      // console.log(response)
+    })
+    .catch(e => {
+      window.alert("Error: " + e);
+    });
+};
 
-export const update = updatedUser=>{
-  return axios.post('http://localhost:5000/users/update',{
-        first_name: updatedUser.first_name,
-        last_name: updatedUser.last_name,
-        gender: updatedUser.gender,
-        email: updatedUser.email,
-        password: updatedUser.password,
-        joining: updatedUser.joining,
-        address: updatedUser.address,
-        status: updatedUser.status,
-        role: updatedUser.role,
-        mobile: updatedUser.mobile
-      }).then(response => {
-        localStorage.removeItem('usertoken')
-        localStorage.setItem('usertoken', response.data)
-        return response.data
-      })
-      .catch(err => {
-        console.log(err)
-      })
-}
+export const fetchUserDetails = token => {
+  return axios
+    .get(`${baseURL}:5000/users/profile`, {
+      headers: {
+        token: token
+      }
+    })
+    .then(response => {
+      console.log("Fetching data");
+    });
+};
+
+export const fetchTeamDetails = token => {
+  return axios
+    .get(`${baseURL}:5000/users/view-member`, {
+      headers: {
+        token: token
+      }
+    })
+    .then(response => {
+      console.log(response.status);
+      if (response.status === 200) {
+        console.log("Entered");
+      } else {
+        localStorage.removeItem("token");
+        window.location.href = "/login";
+      }
+    })
+    .catch(e => {
+      console.log("Error" + e);
+      window.location.href = "/logout";
+      // res.status(500).send(e)
+      // console.log("error")
+    });
+};
+
+export const update = updatedUser => {
+  return axios
+    .post(`${baseURL}:5000/users/update`, {
+      first_name: updatedUser.first_name,
+      last_name: updatedUser.last_name,
+      gender: updatedUser.gender,
+      email: updatedUser.email,
+      password: updatedUser.password,
+      joining: updatedUser.joining,
+      address: updatedUser.address,
+      status: updatedUser.status,
+      role: updatedUser.role,
+      mobile: updatedUser.mobile
+    })
+    .then(response => {
+      localStorage.removeItem("usertoken");
+      localStorage.setItem("usertoken", response.data);
+      return response.data;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
 export const login = user => {
   axios
-    .post('http://localhost:5000/users/login', {
+    .post(`${baseURL}:5000/users/login`, {
       email: user.email,
       password: user.password
     })
     .then(response => {
-      if(response.status === 200) {
-        localStorage.setItem('token', response.data.token)
-        window.location.href = '/dashboard'
+      if (response.status === 200) {
+        localStorage.setItem("token", response.data.token);
+        window.location.href = "/dashboard";
       }
     })
     .catch(err => {
-      window.alert("Error: "+err)
-    })
-}
+      window.alert("Error: " + err);
+    });
+};
 
 export const updatePassword = async user => {
   await axios
-    .post('http://localhost:5000/users/password', {
+    .post(`${baseURL}:5000/users/password`, {
       email: user.email,
       password: user.password
     })
     .then(response => {
-      if(response.status === 200) {
-        return true
-      }
-      else return false
+      if (response.status === 200) {
+        return true;
+      } else return false;
     })
     .catch(err => {
-      window.alert("Error: "+err)
-    })
-}
-
-
-
+      window.alert("Error: " + err);
+    });
+};
 
 // export const logout = user => {
 //   axios
@@ -179,27 +190,25 @@ export const updatePassword = async user => {
 //     })
 // }
 
-
-
-export const teamList = async (activePage, itemCountPerPage) => {
-  let data 
+export const teamList = async () => {
+  let data;
   await axios
-    .get(`http://localhost:5000/users/team-list?page=${activePage}&limit=${itemCountPerPage}`, {
+    .get(`${baseURL}:5000/users/team-list`, {
       headers: {
-        token: localStorage.getItem('token')
+        token: localStorage.getItem("token")
       }
     })
     .then(response => {
-      if(response.status === 200) {
+      if (response.status === 200) {
         // console.log(typeof(response.data))
-        data = [...response.data]
+        data = [...response.data];
         // console.log(data)
         // console.log(response.data)
         // return response.data
       }
     })
     .catch(err => {
-      window.alert("Error: "+err)
-    })
-    return data
-  }
+      window.alert("Error: " + err);
+    });
+  return data;
+};
