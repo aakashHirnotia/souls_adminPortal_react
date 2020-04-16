1ef19ecfb0b7e0910b06b3a3b05a498f00af041c
Ashish Kumar Singh <code.aks.010@gmail.com>
2020-04-16 23:43:48 +0530

    make it better

--- src/_nav.js
+++ src/_nav.js
@@ -1,86 +1,86 @@
 export default {
   items: [
     {
-      name: 'Dashboard',
-      url: '/dashboard',
-      icon: 'icon-speedometer',
+      name: "Dashboard",
+      url: "/dashboard",
+      icon: "icon-speedometer",
       badge: {
-        variant: 'info',
-        text: 'NEW',
+        variant: "info",
+        text: "NEW",
       },
     },
     {
       title: true,
-      name: 'Tables',
-      wrapper: {            // optional wrapper object
-        element: '',        // required valid HTML5 element tag
-        attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
+      name: "Tables",
+      wrapper: {
+        // optional wrapper object
+        element: "", // required valid HTML5 element tag
+        attributes: {}, // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
       },
-      class: ''             // optional class names space delimited list for title item ex: "text-center"
+      class: "", // optional class names space delimited list for title item ex: "text-center"
     },
     {
-      name: 'Customer',
-      url: '/tables/customers',
-      icon: 'icon-people',
-
-
+      name: "Customer",
+      url: "/tables/customers",
+      icon: "icon-people",
     },
     {
-      name: 'Pending orders',
-      url: '/tables/pendingOrders',
-      icon: 'icon-calculator',
+      name: "Pending orders",
+      url: "/tables/pendingOrders",
+      icon: "icon-calculator",
     },
     {
-      name: 'Partners',
-      url: '/tables/ViewPartners',
-      icon: 'icon-people',
+      name: "Partners",
+      url: "/tables/ViewPartners",
+      icon: "icon-people",
     },
     {
-      name: 'Transactions',
-      url: '/tables/transactions',
-      icon: 'icon-pencil',
+      name: "Transactions",
+      url: "/tables/transactions",
+      icon: "icon-pencil",
     },
     {
-      name: 'Assign Partners',
-      url: '/tables/assignPartners',
-      icon: 'icon-people',
+      name: "Assign Partners",
+      url: "/tables/assignPartners",
+      icon: "icon-people",
     },
     {
-        title: true,
-        name: 'Extras',
-        wrapper: {            // optional wrapper object
-          element: '',        // required valid HTML5 element tag
-          attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
-        },
-        class: ''             // optional class names space delimited list for title item ex: "text-center"
+      title: true,
+      name: "Extras",
+      wrapper: {
+        // optional wrapper object
+        element: "", // required valid HTML5 element tag
+        attributes: {}, // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
       },
+      class: "", // optional class names space delimited list for title item ex: "text-center"
+    },
     {
-      name: 'Admin',
+      name: "Admin",
       //url: '/base',
-      icon: 'icon-star',
+      icon: "icon-star",
       children: [
         {
-          name: 'Role',
-          url: '/admin/role',
-          icon: 'icon-cursor',
+          name: "Role",
+          url: "/admin/role",
+          icon: "icon-cursor",
         },
         {
-          name: 'Team Has Role',
-          url: '/admin/teamHasRole',
-          icon: 'icon-cursor',
+          name: "Team Has Role",
+          url: "/admin/teamHasRole",
+          icon: "icon-cursor",
         },
         {
-          name: 'SOULS Settings',
-          url: '/admin/soulsSettings',
-          icon: 'icon-cursor',
+          name: "SOULS Settings",
+          url: "/admin/soulsSettings",
+          icon: "icon-cursor",
         },
         {
-          name: 'Communication Tempelate',
-          url: '/admin/communicationTempelate',
-          icon: 'icon-cursor',
-        }
-      ]
-    }
+          name: "Communication Tempelate",
+          url: "/admin/communicationTempelate",
+          icon: "icon-cursor",
+        },
+      ],
+    },
     // {
     //   title: true,
     //   name: 'Admin',
@@ -95,259 +95,258 @@ export default {
     //   url: '/admin/role',
     //   icon: 'icon-people',
 
-
     // },
     // {
     //   name: 'Team has Role',
     //   url: '/admin/teamHasRole',
     //   icon: 'icon-pencil',
     // }
-//       title: true,
-//       name: 'Components',
-//       wrapper: {
-//         element: '',
-//         attributes: {},
-//       },
-//     },
-//     {
-//       name: 'Base',
-//       url: '/base',
-//       icon: 'icon-puzzle',
-//       children: [
-//         {
-//           name: 'Breadcrumbs',
-//           url: '/base/breadcrumbs',
-//           icon: 'icon-puzzle',
-//         },
-//         {
-//           name: 'Cards',
-//           url: '/base/cards',
-//           icon: 'icon-puzzle',
-//         },
-//         {
-//           name: 'Carousels',
-//           url: '/base/carousels',
-//           icon: 'icon-puzzle',
-//         },
-//         {
-//           name: 'Collapses',
-//           url: '/base/collapses',
-//           icon: 'icon-puzzle',
-//         },
-//         {
-//           name: 'Dropdowns',
-//           url: '/base/dropdowns',
-//           icon: 'icon-puzzle',
-//         },
-//         {
-//           name: 'Forms',
-//           url: '/base/forms',
-//           icon: 'icon-puzzle',
-//         },
-//         {
-//           name: 'Jumbotrons',
-//           url: '/base/jumbotrons',
-//           icon: 'icon-puzzle',
-//         },
-//         {
-//           name: 'List groups',
-//           url: '/base/list-groups',
-//           icon: 'icon-puzzle',
-//         },
-//         {
-//           name: 'Navs',
-//           url: '/base/navs',
-//           icon: 'icon-puzzle',
-//         },
-//         {
-//           name: 'Paginations',
-//           url: '/base/paginations',
-//           icon: 'icon-puzzle',
-//         },
-//         {
-//           name: 'Popovers',
-//           url: '/base/popovers',
-//           icon: 'icon-puzzle',
-//         },
-//         {
-//           name: 'Progress Bar',
-//           url: '/base/progress-bar',
-//           icon: 'icon-puzzle',
-//         },
-//         {
-//           name: 'Switches',
-//           url: '/base/switches',
-//           icon: 'icon-puzzle',
-//         },
-//         {
-//           name: 'Tables',
-//           url: '/base/tables',
-//           icon: 'icon-puzzle',
-//         },
-//         {
-//           name: 'Tabs',
-//           url: '/base/tabs',
-//           icon: 'icon-puzzle',
-//         },
-//         {
-//           name: 'Tooltips',
-//           url: '/base/tooltips',
-//           icon: 'icon-puzzle',
-//         },
-//       ],
-//     },
-//     {
-//       name: 'Buttons',
-//       url: '/buttons',
-//       icon: 'icon-cursor',
-//       children: [
-//         {
-//           name: 'Buttons',
-//           url: '/buttons/buttons',
-//           icon: 'icon-cursor',
-//         },
-//         {
-//           name: 'Button dropdowns',
-//           url: '/buttons/button-dropdowns',
-//           icon: 'icon-cursor',
-//         },
-//         {
-//           name: 'Button groups',
-//           url: '/buttons/button-groups',
-//           icon: 'icon-cursor',
-//         },
-//         {
-//           name: 'Brand Buttons',
-//           url: '/buttons/brand-buttons',
-//           icon: 'icon-cursor',
-//         },
-//       ],
-//     },
-//     {
-//       name: 'Charts',
-//       url: '/charts',
-//       icon: 'icon-pie-chart',
-//     },
-//     {
-//       name: 'Icons',
-//       url: '/icons',
-//       icon: 'icon-star',
-//       children: [
-//         {
-//           name: 'CoreUI Icons',
-//           url: '/icons/coreui-icons',
-//           icon: 'icon-star',
-//           badge: {
-//             variant: 'info',
-//             text: 'NEW',
-//           },
-//         },
-//         {
-//           name: 'Flags',
-//           url: '/icons/flags',
-//           icon: 'icon-star',
-//         },
-//         {
-//           name: 'Font Awesome',
-//           url: '/icons/font-awesome',
-//           icon: 'icon-star',
-//           badge: {
-//             variant: 'secondary',
-//             text: '4.7',
-//           },
-//         },
-//         {
-//           name: 'Simple Line Icons',
-//           url: '/icons/simple-line-icons',
-//           icon: 'icon-star',
-//         },
-//       ],
-//     },
-//     {
-//       name: 'Notifications',
-//       url: '/notifications',
-//       icon: 'icon-bell',
-//       children: [
-//         {
-//           name: 'Alerts',
-//           url: '/notifications/alerts',
-//           icon: 'icon-bell',
-//         },
-//         {
-//           name: 'Badges',
-//           url: '/notifications/badges',
-//           icon: 'icon-bell',
-//         },
-//         {
-//           name: 'Modals',
-//           url: '/notifications/modals',
-//           icon: 'icon-bell',
-//         },
-//       ],
-//     },
-//     {
-//       name: 'Widgets',
-//       url: '/widgets',
-//       icon: 'icon-calculator',
-//       badge: {
-//         variant: 'info',
-//         text: 'NEW',
-//       },
-//     },
-//     {
-//       divider: true,
-//     },
-//     {
-//       title: true,
-//       name: 'Extras',
-//     },
-//     {
-//       name: 'Pages',
-//       url: '/pages',
-//       icon: 'icon-star',
-//       children: [
-//         {
-//           name: 'Login',
-//           url: '/login',
-//           icon: 'icon-star',
-//         },
-//         {
-//           name: 'Register',
-//           url: '/register',
-//           icon: 'icon-star',
-//         },
-//         {
-//           name: 'Error 404',
-//           url: '/404',
-//           icon: 'icon-star',
-//         },
-//         {
-//           name: 'Error 500',
-//           url: '/500',
-//           icon: 'icon-star',
-//         },
-//       ],
-//     },
-//     {
-//       name: 'Disabled',
-//       url: '/dashboard',
-//       icon: 'icon-ban',
-//       attributes: { disabled: true },
-//     },
-//     {
-//       name: 'Download CoreUI',
-//       url: 'https://coreui.io/react/',
-//       icon: 'icon-cloud-download',
-//       class: 'mt-auto',
-//       variant: 'success',
-//       attributes: { target: '_blank', rel: "noopener" },
-//     },
-//     {
-//       name: 'Try CoreUI PRO',
-//       url: 'https://coreui.io/pro/react/',
-//       icon: 'icon-layers',
-//       variant: 'danger',
-//       attributes: { target: '_blank', rel: "noopener" },
-//     },
+    //       title: true,
+    //       name: 'Components',
+    //       wrapper: {
+    //         element: '',
+    //         attributes: {},
+    //       },
+    //     },
+    //     {
+    //       name: 'Base',
+    //       url: '/base',
+    //       icon: 'icon-puzzle',
+    //       children: [
+    //         {
+    //           name: 'Breadcrumbs',
+    //           url: '/base/breadcrumbs',
+    //           icon: 'icon-puzzle',
+    //         },
+    //         {
+    //           name: 'Cards',
+    //           url: '/base/cards',
+    //           icon: 'icon-puzzle',
+    //         },
+    //         {
+    //           name: 'Carousels',
+    //           url: '/base/carousels',
+    //           icon: 'icon-puzzle',
+    //         },
+    //         {
+    //           name: 'Collapses',
+    //           url: '/base/collapses',
+    //           icon: 'icon-puzzle',
+    //         },
+    //         {
+    //           name: 'Dropdowns',
+    //           url: '/base/dropdowns',
+    //           icon: 'icon-puzzle',
+    //         },
+    //         {
+    //           name: 'Forms',
+    //           url: '/base/forms',
+    //           icon: 'icon-puzzle',
+    //         },
+    //         {
+    //           name: 'Jumbotrons',
+    //           url: '/base/jumbotrons',
+    //           icon: 'icon-puzzle',
+    //         },
+    //         {
+    //           name: 'List groups',
+    //           url: '/base/list-groups',
+    //           icon: 'icon-puzzle',
+    //         },
+    //         {
+    //           name: 'Navs',
+    //           url: '/base/navs',
+    //           icon: 'icon-puzzle',
+    //         },
+    //         {
+    //           name: 'Paginations',
+    //           url: '/base/paginations',
+    //           icon: 'icon-puzzle',
+    //         },
+    //         {
+    //           name: 'Popovers',
+    //           url: '/base/popovers',
+    //           icon: 'icon-puzzle',
+    //         },
+    //         {
+    //           name: 'Progress Bar',
+    //           url: '/base/progress-bar',
+    //           icon: 'icon-puzzle',
+    //         },
+    //         {
+    //           name: 'Switches',
+    //           url: '/base/switches',
+    //           icon: 'icon-puzzle',
+    //         },
+    //         {
+    //           name: 'Tables',
+    //           url: '/base/tables',
+    //           icon: 'icon-puzzle',
+    //         },
+    //         {
+    //           name: 'Tabs',
+    //           url: '/base/tabs',
+    //           icon: 'icon-puzzle',
+    //         },
+    //         {
+    //           name: 'Tooltips',
+    //           url: '/base/tooltips',
+    //           icon: 'icon-puzzle',
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       name: 'Buttons',
+    //       url: '/buttons',
+    //       icon: 'icon-cursor',
+    //       children: [
+    //         {
+    //           name: 'Buttons',
+    //           url: '/buttons/buttons',
+    //           icon: 'icon-cursor',
+    //         },
+    //         {
+    //           name: 'Button dropdowns',
+    //           url: '/buttons/button-dropdowns',
+    //           icon: 'icon-cursor',
+    //         },
+    //         {
+    //           name: 'Button groups',
+    //           url: '/buttons/button-groups',
+    //           icon: 'icon-cursor',
+    //         },
+    //         {
+    //           name: 'Brand Buttons',
+    //           url: '/buttons/brand-buttons',
+    //           icon: 'icon-cursor',
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       name: 'Charts',
+    //       url: '/charts',
+    //       icon: 'icon-pie-chart',
+    //     },
+    //     {
+    //       name: 'Icons',
+    //       url: '/icons',
+    //       icon: 'icon-star',
+    //       children: [
+    //         {
+    //           name: 'CoreUI Icons',
+    //           url: '/icons/coreui-icons',
+    //           icon: 'icon-star',
+    //           badge: {
+    //             variant: 'info',
+    //             text: 'NEW',
+    //           },
+    //         },
+    //         {
+    //           name: 'Flags',
+    //           url: '/icons/flags',
+    //           icon: 'icon-star',
+    //         },
+    //         {
+    //           name: 'Font Awesome',
+    //           url: '/icons/font-awesome',
+    //           icon: 'icon-star',
+    //           badge: {
+    //             variant: 'secondary',
+    //             text: '4.7',
+    //           },
+    //         },
+    //         {
+    //           name: 'Simple Line Icons',
+    //           url: '/icons/simple-line-icons',
+    //           icon: 'icon-star',
+    //         },
+    //       ],
+    //     },
+    {
+      name: "Notifications",
+      url: "/notifications",
+      icon: "icon-bell",
+      children: [
+        {
+          name: "Alerts",
+          url: "/notifications/alerts",
+          icon: "icon-bell",
+        },
+        {
+          name: "Badges",
+          url: "/notifications/badges",
+          icon: "icon-bell",
+        },
+        {
+          name: "Modals",
+          url: "/notifications/modals",
+          icon: "icon-bell",
+        },
+      ],
+    },
+    //     {
+    //       name: 'Widgets',
+    //       url: '/widgets',
+    //       icon: 'icon-calculator',
+    //       badge: {
+    //         variant: 'info',
+    //         text: 'NEW',
+    //       },
+    //     },
+    //     {
+    //       divider: true,
+    //     },
+    //     {
+    //       title: true,
+    //       name: 'Extras',
+    //     },
+    //     {
+    //       name: 'Pages',
+    //       url: '/pages',
+    //       icon: 'icon-star',
+    //       children: [
+    //         {
+    //           name: 'Login',
+    //           url: '/login',
+    //           icon: 'icon-star',
+    //         },
+    //         {
+    //           name: 'Register',
+    //           url: '/register',
+    //           icon: 'icon-star',
+    //         },
+    //         {
+    //           name: 'Error 404',
+    //           url: '/404',
+    //           icon: 'icon-star',
+    //         },
+    //         {
+    //           name: 'Error 500',
+    //           url: '/500',
+    //           icon: 'icon-star',
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       name: 'Disabled',
+    //       url: '/dashboard',
+    //       icon: 'icon-ban',
+    //       attributes: { disabled: true },
+    //     },
+    //     {
+    //       name: 'Download CoreUI',
+    //       url: 'https://coreui.io/react/',
+    //       icon: 'icon-cloud-download',
+    //       class: 'mt-auto',
+    //       variant: 'success',
+    //       attributes: { target: '_blank', rel: "noopener" },
+    //     },
+    //     {
+    //       name: 'Try CoreUI PRO',
+    //       url: 'https://coreui.io/pro/react/',
+    //       icon: 'icon-layers',
+    //       variant: 'danger',
+    //       attributes: { target: '_blank', rel: "noopener" },
+    //     },
   ],
- };
+};
